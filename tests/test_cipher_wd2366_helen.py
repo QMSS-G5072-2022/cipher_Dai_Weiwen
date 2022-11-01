f38ede0fad30074df6c13c0b9676cf442d02b685
StarHelen88 <113069219+StarHelen88@users.noreply.github.com>
2022-11-01 19:57:05 -0400

    Added working version of catbind function

--- tests/test_cipher_wd2366_helen.py
+++ tests/test_cipher_wd2366_helen.py
@@ -1,1 +1,12 @@
 from cipher_wd2366_helen import cipher_wd2366_helen
+
+import pytest
+@pytest.mark.parametrize('text, shift, expected', [
+    ('Helen', 1, 'Ifmfo'),
+    ('helen', 1, 'ifmfo'),
+    ('HELEN', 1, 'IFMFO'),
+    ('My name is Helen', 1, 'Nz obnf jt Ifmfo'),
+])
+
+def test_cipher(text, shift, expected):
+    assert cipher_wd2366_helen.cipher(text, shift) == expected, 'Function cipher not pass.'
