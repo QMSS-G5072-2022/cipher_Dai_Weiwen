42fd778e88d8f38e5f3838b9b1c58df2a1ea7491
StarHelen88 <113069219+StarHelen88@users.noreply.github.com>
2022-11-01 20:18:48 -0400

    hw07

--- tests/test_cipher_wd2366_helen.py
+++ tests/test_cipher_wd2366_helen.py
@@ -5,8 +5,7 @@ import pytest
     ('Helen', 1, 'Ifmfo'),
     ('helen', 1, 'ifmfo'),
     ('HELEN', 1, 'IFMFO'),
-    ('My name is Helen', 1, 'Nz obnf jt Ifmfo'),
-])
+    ('My name is Helen', 1, 'Nz obnf jt Ifmfo')])
 
 def test_cipher(text, shift, expected):
     assert cipher_wd2366_helen.cipher(text, shift) == expected, 'Function cipher not pass.'
